cb18832f77c5b3facc36d77039206cb5dcd480f1
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-27 22:50:26 +0600

    creating routing object

--- modules/module-44/price-options/src/conponents/CustomNav.jsx
+++ modules/module-44/price-options/src/conponents/CustomNav.jsx
@@ -1,5 +1,13 @@
 
 const CustomNav = () => {
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/services', name: 'Services' },
+        { id: 4, path: '/contact', name: 'Contact' },
+        { id: 5, path: '/profile/:username', name: 'Profile' },
+    ];
+
     return (
         <nav>
 
