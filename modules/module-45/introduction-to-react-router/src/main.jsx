ad4e29f3fe91150f2f7726410c5a83f880cd65e5
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-28 23:17:00 +0600

    dibugging why it is not working

--- modules/module-45/introduction-to-react-router/src/main.jsx
+++ modules/module-45/introduction-to-react-router/src/main.jsx
@@ -2,22 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World!</div>
-  },
-  {
-    path: "/about",
-    element: <div>this is about page</div>
-  }
-])
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello World!</div>
+//   },
+//   {
+//     path: "/about",
+//     element: <div>this is about page</div>
+//   }
+// ])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App></App>
+    {/* <RouterProvider router={router}></RouterProvider> */}
   </StrictMode>,
 )
