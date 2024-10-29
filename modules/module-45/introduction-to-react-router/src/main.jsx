d160470733d26c64beb1edfa3d56ec0c58d5306e
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-29 14:58:06 +0600

    page rendering based on url path

--- modules/module-45/introduction-to-react-router/src/main.jsx
+++ modules/module-45/introduction-to-react-router/src/main.jsx
@@ -2,23 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <div>Hello World!</div>
-//   },
-//   {
-//     path: "/about",
-//     element: <div>this is about page</div>
-//   }
-// ])
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World!</div>
+  },
+  {
+    path: "/about",
+    element: <div>this is about page</div>
+  },
+  {
+    path: '/contact',
+    element: <div>this is contact page</div>
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App></App>
-    {/* <RouterProvider router={router}></RouterProvider> */}
-  </StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
 )
