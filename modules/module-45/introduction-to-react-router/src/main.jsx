28a142b620d61b1cf9b1dc5b34488e99cd0c7037
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-29 22:30:33 +0600

    deleting component folder

--- modules/module-45/introduction-to-react-router/src/main.jsx
+++ modules/module-45/introduction-to-react-router/src/main.jsx
@@ -6,6 +6,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App />
   </StrictMode>
 )
