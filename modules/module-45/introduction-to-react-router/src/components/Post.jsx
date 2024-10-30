e793cf2f2fd19e36526ca29ffa6a5306e0106a89
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-30 21:20:28 +0600

    learning useNavigate

--- modules/module-45/introduction-to-react-router/src/components/Post.jsx
+++ modules/module-45/introduction-to-react-router/src/components/Post.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({ post }) => {
     const { title, body, id } = post;
+    const navigetToBody = useNavigate();
+
+    const handleBody = () => {
+        navigetToBody(`/posts/${id}`)
+    }
     return (
         <div className="border-2 border-sky-400 py-5 m-1 rounded-lg">
             <h3>{title}</h3>
-            <Link to={`/posts/${id}`}><button className="mt-2">Body</button></Link>
+            <button onClick={handleBody} className="mt-2">Body</button>
         </div>
     );
 };
--- modules/module-45/introduction-to-react-router/src/components/PostBody.jsx
+++ modules/module-45/introduction-to-react-router/src/components/PostBody.jsx
@@ -1,10 +1,15 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostBody = () => {
     const body = useLoaderData();
+    const backGo = useNavigate()
+    const handleBack = () => {
+        backGo(-1)
+    }
     return (
-        <div>
+        <div className="border rounded-md py-3">
             <p className="w-[70%] mx-auto">{body.body}</p>
+            <button onClick={handleBack} className="mt-2">Back</button>
         </div>
     );
 };
