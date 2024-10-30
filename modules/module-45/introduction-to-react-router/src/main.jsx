800785845b7b49a511c6c50812418a2a53f1582d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-30 14:27:10 +0600

    recape module 45

--- modules/module-45/introduction-to-react-router/src/main.jsx
+++ modules/module-45/introduction-to-react-router/src/main.jsx
@@ -2,10 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>hello from react router</div>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>
 )
