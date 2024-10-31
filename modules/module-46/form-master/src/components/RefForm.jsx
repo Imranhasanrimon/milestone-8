438b83146c16b11afc8f24cc409e8b6aa7979fa2
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-31 18:21:13 +0600

    module 46 is done

--- modules/module-46/form-master/src/components/RefForm.jsx
+++ modules/module-46/form-master/src/components/RefForm.jsx
@@ -2,23 +2,23 @@ import { useEffect, useRef } from "react";
 
 const RefForm = () => {
     const nameRef = useRef(null);
-
-    const handleOnSubmit = e =>{
+    const emailRef = useRef(null)
+    const handleOnSubmit = e => {
         e.preventDefault();
         console.log(nameRef.current.value);
     }
 
-    useEffect(()=>{
-nameRef.current.focus()
-    },[])
-    
+    useEffect(() => {
+        emailRef.current.focus();
+    }, [])
+
     return (
         <div>
             <form onSubmit={handleOnSubmit} className="space-y-2">
                 <input ref={nameRef} type="text" name="name" placeholder='name' id="" /> <br />
-                <input type="email" name="email" placeholder="email" id="" /> <br />
+                <input ref={emailRef} type="email" defaultValue='imran' name="email" placeholder="email" id="" /> <br />
                 <input type="password" name="password" placeholder='pass' id="" /> <br />
-               <input type="submit" className="btn" value="Submit" name="" id="" />
+                <input type="submit" className="btn" value="Submit" name="" id="" />
             </form>
         </div>
     );
