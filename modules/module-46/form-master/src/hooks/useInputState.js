4f346c3a5971f598f4245ec25e2ff6c7fdafcd1d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-31 22:05:56 +0600

    replacing the custom hook by object

--- modules/module-46/form-master/src/hooks/useInputState.js
+++ modules/module-46/form-master/src/hooks/useInputState.js
@@ -3,10 +3,13 @@ import { useState } from "react"
 const useInputState = (defaultValue = null) => {
     const [value, setValue] = useState(defaultValue);
 
-    const handleChange = e => {
+    const onChange = e => {
         setValue(e.target.value);
     }
-    return [value, handleChange]
+    return {
+        value,
+        onChange
+    }
 }
 
 export default useInputState;
