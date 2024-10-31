222df6a168b80353cfdbb7578bf29a27ad0693ad
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-10-31 21:26:55 +0600

    creating custom hook

--- modules/module-46/form-master/src/components/RefForm.jsx
+++ modules/module-46/form-master/src/components/RefForm.jsx
@@ -6,6 +6,7 @@ const RefForm = () => {
     const handleOnSubmit = e => {
         e.preventDefault();
         console.log(nameRef.current.value);
+        console.log(emailRef.current.value);
     }
 
     useEffect(() => {
@@ -15,9 +16,9 @@ const RefForm = () => {
     return (
         <div>
             <form onSubmit={handleOnSubmit} className="space-y-2">
-                <input ref={nameRef} type="text" name="name" placeholder='name' id="" /> <br />
-                <input ref={emailRef} type="email" defaultValue='imran' name="email" placeholder="email" id="" /> <br />
-                <input type="password" name="password" placeholder='pass' id="" /> <br />
+                <input ref={nameRef} type="text" name="name" placeholder='name' id="1" /> <br />
+                <input ref={emailRef} type="email" defaultValue='imran' name="email" placeholder="email" id="2" /> <br />
+                <input type="password" name="password" placeholder='pass' id="3" /> <br />
                 <input type="submit" className="btn" value="Submit" name="" id="" />
             </form>
         </div>
