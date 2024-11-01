8f0f89d7be766861ac361bbbac3daf019048d20b
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-01 15:29:36 +0600

    making details book route

--- modules/module-47/boi-poka-book-vibe/src/components/BookDetail.jsx
+++ modules/module-47/boi-poka-book-vibe/src/components/BookDetail.jsx
@@ -1,8 +1,18 @@
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetail = () => {
+    const { bookId } = useParams();
+    const id = parseInt(bookId);
+    const data = useLoaderData();
+    const book = data.find(boo => boo.bookId === id);
+    const { bookId: currentBookId, image } = book
     return (
-        <div>
-            <h1>book details</h1>
+        <div className="my-8">
+            <h1>book details: {bookId}</h1>
+            <img className="w-36" src={image} alt="" />
+            <br />
+            <button className="btn border border-gray-400 mr-6">Mark as Read</button>
+            <button className="btn btn-primary">Add to WhishList</button>
         </div>
     );
 };
