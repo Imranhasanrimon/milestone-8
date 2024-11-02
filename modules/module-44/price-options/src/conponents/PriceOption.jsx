267bed7d5e5ae14703d5a4c8f16a8e9eb35f9e37
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-02 11:11:37 +0600

    aligning cards perfectly by flex grow

--- modules/module-44/price-options/src/conponents/PriceOption.jsx
+++ modules/module-44/price-options/src/conponents/PriceOption.jsx
@@ -3,14 +3,14 @@ import { RiFocus2Line } from "react-icons/ri";
 const PriceOption = ({ option }) => {
     const { price, name, features } = option;
     return (
-        <div className="bg-sky-500 text-white w-full rounded-md p-4 space-y-3">
+        <div className="bg-sky-500 text-white w-full rounded-md p-4 space-y-3 flex flex-col">
             <h2 className="text-center">
                 <span className="text-6xl font-semibold">{price}</span>
                 <span>/mon</span>
             </h2>
             <h3 className="text-center text-xl border rounded-md py-1">{name}</h3>
 
-            <ul>
+            <ul className="flex-grow">
                 {
                     features.map((feature, indx) => <li className="flex items-center" key={indx}>{<RiFocus2Line className="mr-2" />}{feature}</li>)
                 }
