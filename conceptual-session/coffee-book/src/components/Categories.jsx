4fd63fe429c2d8e03ee3962c3847c75b9ce1b498
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-03 23:08:29 +0600

    installing propTypes

--- conceptual-session/coffee-book/src/components/Categories.jsx
+++ conceptual-session/coffee-book/src/components/Categories.jsx
@@ -1,6 +1,9 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Categories = ({ categories }) => {
+
     return (
         <div role="tablist" className="tabs tabs-lifted">
             {
@@ -9,5 +12,7 @@ const Categories = ({ categories }) => {
         </div>
     );
 };
-
+Categories.propTypes = {
+    categories: PropTypes.array.isRequired
+}
 export default Categories;
--- conceptual-session/coffee-book/src/components/CoffeeCards.jsx
+++ conceptual-session/coffee-book/src/components/CoffeeCards.jsx
@@ -1,14 +1,19 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 const CoffeeCards = () => {
     const { category } = useParams();
-    console.log(category);
     const data = useLoaderData();
+    const [coffees, setCoffees] = useState([]);
+    useEffect(() => {
+        const filteredByCategory = [...data].filter(coffee => coffee.category === category);
+        setCoffees(filteredByCategory)
+    }, [category, data])
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {
-                data.map(coffee => <Card key={coffee.id} coffee={coffee}></Card>)
+                coffees.map(coffee => <Card key={coffee.id} coffee={coffee}></Card>)
             }
         </div>
     );
