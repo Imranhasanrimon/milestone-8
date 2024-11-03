f5ee1e06156ee653a5dbb6260b26c445a8b070a6
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-03 09:15:56 +0600

    creating several components

--- conceptual-session/coffee-book/src/routes/Routes.jsx
+++ conceptual-session/coffee-book/src/routes/Routes.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom"
-import App from "../App"
+import MainLayout from "../layouts/MainLayout";
 const routes = createBrowserRouter([
     {
         path: '/',
-        element: <App></App>
+        element: <MainLayout></MainLayout>
     }
 ])
 
