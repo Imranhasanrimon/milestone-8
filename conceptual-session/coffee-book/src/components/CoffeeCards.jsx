3671d5e936a2a34f8946b6d56f5b70ecc5fe42d9
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-04 10:05:38 +0600

    view all button navigation by useNavigate hook

--- conceptual-session/coffee-book/src/components/CoffeeCards.jsx
+++ conceptual-session/coffee-book/src/components/CoffeeCards.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 import Card from "./Card";
 import { useEffect, useState } from "react";
 
@@ -6,22 +6,26 @@ const CoffeeCards = () => {
     const { category } = useParams();
     const data = useLoaderData();
     const [coffees, setCoffees] = useState([]);
+    const goToCoffees = useNavigate();
 
     useEffect(() => {
         if (category) {
             const filteredByCategory = [...data].filter(coffee => coffee.category === category);
             setCoffees(filteredByCategory)
         } else {
-            setCoffees(data)
+            setCoffees(data.slice(0, 6))
         }
     }, [category, data])
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {
-                coffees.map(coffee => <Card key={coffee.id} coffee={coffee}></Card>)
-            }
-        </div>
+        <>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {
+                    coffees.map(coffee => <Card key={coffee.id} coffee={coffee}></Card>)
+                }
+            </div>
+            <button onClick={() => goToCoffees('/coffees')} className="btn btn-warning">View All</button>
+        </>
     );
 };
 
